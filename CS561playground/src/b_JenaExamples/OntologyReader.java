51b474a7a141c58a2ffd75f97ed80eff71bd9efa
YannisTzitzikas <tzitzik@ics.forth.gr>
2022-02-26 22:17:55 +0200

    Add a call to a visualization library

--- CS561playground/src/b_JenaExamples/OntologyReader.java
+++ CS561playground/src/b_JenaExamples/OntologyReader.java
@@ -3,11 +3,15 @@
  */
 package b_JenaExamples;
 
+import java.awt.Desktop;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.net.URL;
 
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+
+import java.net.URI;
 
 /**
  * @author Yannis Tzitzikas (yannistzitzik@gmail.com)
@@ -30,14 +34,38 @@ public class OntologyReader {
         System.out.println(model);
         
 	}
+	 
+	/**
+	 * Calls webvowl to visualize an ontology
+	 * @param url  the url of an ontology (should be web accessible)
+	 */
+	void visualize(String url) {
+		//String v7 = "https://service.tib.eu/webvowl/#iri=https://cidoc-crm.org/rdfs/7.1.1/CIDOC_CRM_v7.1.1.rdfs";
+		//String v6 ="https://service.tib.eu/webvowl/#iri=http://www.cidoc-crm.org/sites/default/files/cidoc_crm_v6.2-2018April.rdfs";
+		
+		String urlwithParam = "https://service.tib.eu/webvowl/#iri="+url;
+		
+		try {
+			if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+			    Desktop.getDesktop().browse(new URI(urlwithParam));
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+			
+	}
 	
 	public static void main(String[] lala) {
+		// two versions of the CIDOC CRM ontology 
 		String v2018 = "/ontologies/cidoc_crm_v6.2-2018April.rdfs.xml";
 		String v2021 = "/ontologies/CIDOC_CRM_v7.1.1.rdfs.xml";
 		
 		OntologyReader a = new OntologyReader();
 		//a.readFromPath(v2018);
 		a.readFromPath(v2021);
+		
+		a.visualize("https://cidoc-crm.org/rdfs/7.1.1/CIDOC_CRM_v7.1.1.rdfs");
 		
 	}
    
